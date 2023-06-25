dbcdd13c193e7e6f592f66b9bac0c8ef948909a4
Jonica Lamere <jonicalamere@gmail.com>
2023-06-25 01:25:16 +0000

    EC2

--- ec2/create_security_group.py
+++ ec2/create_security_group.py
@@ -1,4 +1,11 @@
 import boto3
 
-ec2 = boto3.client(ec2)
+ec2 = boto3.client('ec2')
 
+response = ec2.create_security_group(
+    Description='SG from boto3',
+    GroupName='boto3-security-group',
+    VpcId='vpc-06c068119b33ff027',
+)
+
+print(response["GroupId"])
